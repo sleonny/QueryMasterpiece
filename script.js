efab5a82d27862bd66008d4261da7105c1faffd2
Sean L <leonardmsean@gmail.com>
2023-10-18 20:53:16 -0400

    attempting to hit the artsy api, not having luck

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
-document.getElementById("search-button").addEventListener(
-  click,
-  fetch("https://api.artsy.net/api")
+document.getElementById("search-button").addEventListener("click", function () {
+  const searchTerm = document.getElementById("searchInput").value;
+
+  const apiEndpoint = `https://api.artsy.net/api/artists?search=${encodeURIComponent(
+    searchTerm
+  )}`;
+
+  fetch(apiEndpoint)
     .then((response) => {
       return response.json();
     })
     .then((data) => {
       console.log(data);
     })
-);
+    .catch((error) => {
+      console.error("Error:", error);
+    });
+});
